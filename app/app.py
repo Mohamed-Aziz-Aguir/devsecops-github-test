4c462fa7f6fae2e1d6fbf2bc1cce755af13839c7
Mohamed-Aziz-Aguir <mohamedaziz.aguir@outlook.com>
2026-04-13 15:12:16 +0100

    changing app

--- app/app.py
+++ app/app.py
@@ -2,15 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# routes
+
 @app.route("/")
 def home():
     return jsonify({"message": "secure devsecops app running"})
+
 
 @app.route("/health")
 def health():
     return jsonify({"status": "ok"})
 
-# main
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
